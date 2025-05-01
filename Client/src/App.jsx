6c57177fc84ca5eeac002398625a14efe986ab68
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-05-02 00:20:15 +0530

    Working on hotel booking

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,20 @@
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar"
+import Home from "./Pages/Home"
+import {Routes, Route, useLocation} from "react-router-dom";
+
 function App() {
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <>
-      
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      </div>
     </>
   )
 }
