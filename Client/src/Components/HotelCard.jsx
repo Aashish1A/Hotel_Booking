3a87079439700e3cf5df27d68a3b500116c43e76
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-06-08 15:34:09 +0530

    Creating hotelCard Component

--- Client/src/Components/HotelCard.jsx
+++ Client/src/Components/HotelCard.jsx
@@ -5,6 +5,7 @@ const HotelCard = ({room, index}) => {
   return (
     <Link  to={`/rooms/${room._id}`} onCanPlay={() => scrollTo(0, 0)} key={room._id}>
         <img src={room.images[0]} alt="" />
+        
     </Link>
   )
 }
