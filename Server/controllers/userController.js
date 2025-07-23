6543f4ae052756b8fafde9879fd8a54ab74ba997
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-07-23 18:05:05 +0530

    feat(user): add user controller, model and routes

---
+++ Server/controllers/userController.js
@@ -0,0 +1,30 @@
+// Get /api/user
+export const getUserData = async (req, res) => {
+    try {
+        const role = req.user.role;
+        const recentSearchedCities = req.user.recentSearchedCities;
+        res.json({success: true, role, recentSearchedCities});
+    } catch (error) {
+        res.status(500).json({ success: false, message: error });
+    }
+}
+
+// Store User Recent Searched Cities
+export const storeRecentSearchedCities = async (req, res) => {
+    try {
+        const {recentSearchCity} = req.body;
+        const user = await req.user;
+
+        if(user.recentSearchedCities.length < 3) {
+            user.recentSearchedCities.push(recentSearchCity); // Remove the oldest city
+        }else{
+            user.recentSearchedCities.shift(); // Remove the oldest city
+            user.recentSearchedCities.push(recentSearchCity); // Add the new city
+        }
+        await user.save();
+        res.json({success: true, message: 'Recent searched cities updated successfully'});
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
