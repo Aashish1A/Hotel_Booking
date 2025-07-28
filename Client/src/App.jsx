bb1dea1bf83d9a695a8009bb6e448b774162b0df
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-07-29 00:28:11 +0530

    refactor(app, main): just added context provide and done little changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,9 @@
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
-import Navbar from "./Components/Navbar"
+import Navbar from "./Components/Navbar";
 import AllRooms from "./Pages/AllRooms";
-import Home from "./Pages/Home"
-import {Routes, Route, useLocation} from "react-router-dom";
+import Home from "./Pages/Home";
+import { Routes, Route, useLocation } from "react-router-dom";
 import RoomDetails from "./Pages/RoomDetails";
 import MyBooking from "./Pages/MyBooking";
 import HotelReg from "./Components/HotelReg";
@@ -11,31 +11,34 @@ import Layout from "./Pages/hotelOwner/Layout";
 import DashBoard from "./Pages/hotelOwner/DashBoard";
 import AddRoom from "./Pages/hotelOwner/AddRoom";
 import ListRoom from "./Pages/hotelOwner/ListRoom";
+import { Toaster } from "react-hot-toast";
+import { useAppContext } from "./context/appContext";
 
 function App() {
-
   const isOwnerPath = useLocation().pathname.includes("owner");
+  const { showHotelReg } = useAppContext();
 
   return (
     <>
+      <Toaster />
       {!isOwnerPath && <Navbar />}
-      {false && <HotelReg />}
+      {showHotelReg && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/rooms' element={<AllRooms />} />
-        <Route path='/rooms/:id' element={<RoomDetails />} />
-        <Route path='/my-bookings' element={<MyBooking />} />
-        <Route path="/owner" element={<Layout />}>
-          <Route index element={<DashBoard />} />
-          <Route path="add-room" element={<AddRoom />} />
-          <Route path="list-room" element={<ListRoom />} />
-        </Route>
-      </Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBooking />} />
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<DashBoard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+          </Route>
+        </Routes>
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
