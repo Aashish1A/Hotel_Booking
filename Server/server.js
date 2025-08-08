805fecf483d4aeb6438065346252d8e19cdd24dd
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-08-08 23:36:09 +0530

    refactor(server): some minor changes done in server file

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv/config';
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
-import clerkWebhook from './controllers/clerkWebHooks.js';
-import userRouter from './routes/userRoutes.js';
-import connectCloudinary from './configs/cloudinary.js';
-import roomRouter from './routes/roomRoutes.js';
-import hotelRouter from './routes/hotelRoutes.js';
-import bookingRouter from './routes/bookingRoutes.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhook from "./controllers/clerkWebHooks.js";
+import userRouter from "./routes/userRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 connectDB();
 connectCloudinary();
@@ -19,17 +19,17 @@ const PORT = process.env.PORT;
 // Enable CORS
 app.use(cors());
 
-// 🛡️ Clerk Auth Middleware
-app.use(clerkMiddleware());
-
 // ✅ Clerk Webhook: RAW BODY middleware for verification
 app.use("/api/clerk", express.raw({ type: "*/*" }), clerkWebhook);
 
 // ✅ Parse JSON body after raw body setup
 app.use(express.json());
 
+// 🛡️ Clerk Auth Middleware
+app.use(clerkMiddleware());
+
 // API Routes
-app.get('/', (req, res) => res.send('Welcome to the Hotel Booking API'));
+app.get("/", (req, res) => res.send("Welcome to the Hotel Booking API"));
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/rooms", roomRouter);
@@ -37,5 +37,5 @@ app.use("/api/bookings", bookingRouter);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
