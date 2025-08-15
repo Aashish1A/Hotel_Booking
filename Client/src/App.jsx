a99537c160261913491aec13fae5e38bf1410ded
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-08-15 23:05:16 +0530

    refactor(app, home): Add comments and all

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,21 +14,31 @@ import ListRoom from "./Pages/hotelOwner/ListRoom";
 import { Toaster } from "react-hot-toast";
 import { useAppContext } from "./context/appContext";
 
+/**
+ * Main App Component
+ * Handles routing and global layout based on user type (customer vs hotel owner)
+ */
 function App() {
+  // Check if current route is hotel owner dashboard
   const isOwnerPath = useLocation().pathname.includes("owner");
   const { showHotelReg } = useAppContext();
 
   return (
     <>
       <Toaster />
+      {/* Show main navbar only for customer pages */}
       {!isOwnerPath && <Navbar />}
+      {/* Hotel registration modal overlay */}
       {showHotelReg && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
+          {/* Customer routes */}
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBooking />} />
+
+          {/* Hotel owner routes with nested layout */}
           <Route path="/owner" element={<Layout />}>
             <Route index element={<DashBoard />} />
             <Route path="add-room" element={<AddRoom />} />
--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Hero from '../Components/Hero'
-import FeaturedDestination from '../Components/FeaturedDestination'
-import ExclusiveOffer from '../Components/ExclusiveOffer'
-import UserTestimonial from '../Components/UserTestimonial'
-import NewsLetter from '../Components/NewsLetter'
+import React from "react";
+import Hero from "../Components/Hero";
+import FeaturedDestination from "../Components/FeaturedDestination";
+import ExclusiveOffer from "../Components/ExclusiveOffer";
+import UserTestimonial from "../Components/UserTestimonial";
+import NewsLetter from "../Components/NewsLetter";
+import RecommendedHotels from "../Components/RecommendedHotels";
 
+/**
+ * Home Page Component
+ * Landing page that displays all main sections of the hotel booking site
+ */
 const Home = () => {
   return (
     <>
-        <Hero />
-        <FeaturedDestination />
-        <ExclusiveOffer />
-        <UserTestimonial />
-        <NewsLetter />
+      <Hero />
+      <RecommendedHotels />
+      <FeaturedDestination />
+      <ExclusiveOffer />
+      <UserTestimonial />
+      <NewsLetter />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
