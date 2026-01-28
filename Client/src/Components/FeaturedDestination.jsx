c7a5ee6101f77167a8f5a10758a7585a798a5e8d
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2026-01-29 00:43:53 +0530

    refactor(FeaturedDestination): Simplify JSX structure and improve readability

--- Client/src/Components/FeaturedDestination.jsx
+++ Client/src/Components/FeaturedDestination.jsx
@@ -4,21 +4,21 @@ import Title from './Title'
 import { useAppContext } from '../context/appContext'
 
 const FeaturedDestination = () => {
-
     const {rooms, navigate} = useAppContext();
 
+    return rooms.length > 0 &&  (
+        <div className='flex flex-col items-center px-6 md:px-17 lg:px-24 bg-slate-50 py-20'>
+            <Title title='Featured Destination' subTitle='Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences.' />
+            <div className='flex flex-wrap justify-center gap-6 mt-20 w-full max-w-7xl'>
+                {rooms.slice(0,4).map((room, index) => (
+                    <div key={room._id} className='w-full md:w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)] border border-slate-200 rounded-xl overflow-hidden'>
+                        <HotelCard room={room} index={index}/>
+                    </div>
+                ))}
+            </div>
 
-  return rooms.length > 0 &&  (
-    <div className='flex flex-col items-center px-6 md:px-17 lg:px-24 bg-slate-50 py-20'>
-        <Title title='Featured Destination' subTitle='Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences.' />
-        <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-            {rooms.slice(0,4).map((room, index) => (
-                <HotelCard key={room._id} room={room} index={index}/>
-            ))}
+            <button onClick={() => {navigate("/rooms"); scrollTo(0,0)} } className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>View All Destination</button>
         </div>
-
-        <button onClick={() => {navigate("/rooms"); scrollTo(0,0)} } className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>View All Destination</button>
-    </div>
   )
 }
 
